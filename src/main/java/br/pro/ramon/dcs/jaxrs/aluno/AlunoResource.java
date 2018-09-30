4093820488eb6f3b233fcdadec49854f9d5d44eb
Ramon Chiara <ramonchiara@gmail.com>
2018-09-29 23:34:21 -0300

    Formulário com AJAX (input - só o valor).

--- src/main/java/br/pro/ramon/dcs/jaxrs/aluno/AlunoResource.java
+++ src/main/java/br/pro/ramon/dcs/jaxrs/aluno/AlunoResource.java
@@ -10,10 +10,10 @@ public class AlunoResource {
 
     @GET
     @Produces("text/plain; charset=UTF-8")
-    public String getMedia(@QueryParam("nome") String nome, @QueryParam("p1") double p1, @QueryParam("p2") double p2) {
+    public double getMedia(@QueryParam("nome") String nome, @QueryParam("p1") double p1, @QueryParam("p2") double p2) {
         Aluno aluno = new Aluno(nome, p1, p2);
 
-        return "A média do aluno " + aluno.getNome() + " é " + aluno.getMedia() + ".";
+        return aluno.getMedia();
     }
 
 }
